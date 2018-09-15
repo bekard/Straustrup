fd8cb43cc5700663b87ac757d8e638e410b05b45
Anton Arefyev <42998026+bekard@users.noreply.github.com>
2018-09-15 15:03:32 +0300

    Add files via upload

---
+++ Chapter 4/Exercsie 12.cpp
@@ -0,0 +1,28 @@
+#define _SILENCE_STDEXT_HASH_DEPRECATION_WARNINGS
+#include "std_lib_facilities.h"
+
+vector<int> primes;
+
+bool is_prime(int x)
+{
+	for (int p = 0; p < primes.size(); ++p)
+		if (x % primes[p] == 0) return false;
+	return true;
+}
+
+int main()
+{
+	int max = 0;
+	cin >> max;
+
+	primes.push_back(2);
+
+	for (int i = 3; i <= max; ++i)
+		if (is_prime(i)) primes.push_back(i);
+
+	cout << "Primes: \n";
+	for (int p = 0; p < primes.size(); ++p)
+		cout << primes[p] << endl;
+
+	return 0;
+}
